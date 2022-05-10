9539b056a6047e125a3b05ee1b12ff35cb37153a
Andy Lai <alai@assurance.com>
2022-05-10 16:02:44 -0400

    Add another test without coverage

--- construct/dummy.py
+++ construct/dummy.py
@@ -1,23 +1,21 @@
 # This is a dummy file to check test coverage T
 
-class Dummy:
-    @staticmethod
-    def dummy(input: bool):
-        if input:
-            return 1
-        else:
-            return 2
+def dummy(input: bool):
+    if input:
+        return 1
+    else:
+        return 2
 
-    @staticmethod
-    def dummy2(input: bool):
-        if input:
-            return 1
-        else:
-            return 2
 
-    @staticmethod
-    def dummy3(input: bool):
-        if input:
-            return 1
-        else:
-            return 2
+def dummy2(input: bool):
+    if input:
+        return 1
+    else:
+        return 2
+
+
+def dummy3(input: bool):
+    if input:
+        return 1
+    else:
+        return 2
