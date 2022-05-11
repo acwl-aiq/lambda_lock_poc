3d3f634575dd98887d63ec26be4b49dfcdac3db2
Andy Lai <alai@assurance.com>
2022-05-10 22:53:42 -0400

    Added more uncovered tests

--- construct/dummy.py
+++ construct/dummy.py
@@ -2,37 +2,31 @@
 
 class Dummy:
 
-    def __init__(self):
-        print("x")
-
-    def m_dummy(self, input: bool, input2: bool, input3: bool):
-        if input:
-            return 3
-        if input2:
-            return 5
-
     @staticmethod
     def dummy(input: bool, input2: bool, input3: bool):
+        if input:
+            return 1
+        else:
+            return 2
+
+    @staticmethod
+    def dummy2(input: bool, input2: bool, input3: bool):
         if input2:
             return 3
-
         if input3:
             return 4
-
         if input:
             return 1
         else:
             return 2
 
-    @staticmethod
-    def dummy2(input: bool):
-        if input:
-            return 1
-        else:
-            return 2
 
     @staticmethod
-    def dummy3(input: bool):
+    def dummy3(input: bool, input2: bool, input3: bool):
+        if input2:
+            return 3
+        if input3:
+            return 4
         if input:
             return 1
         else:
